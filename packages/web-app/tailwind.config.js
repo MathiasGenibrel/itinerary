5aec489e9dd36a7876c5d3c0183a00326c75a277
Mathias Genibrel <mathias.geni@gmail.com>
2023-09-25 22:05:26 +0200

    ♻️ Change Astro for NextJS to use UI library.

--- packages/web-app/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-import {nextui} from "@nextui-org/react";
-
-export default {
-  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}", "../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
-  plugins: [nextui()]
-}
-
