faa50419d7820a7863de96f2088068bce17ef954
Mathias Genibrel <mathias.geni@gmail.com>
2023-09-27 22:57:50 +0200

    ♻️ Component minification
    Minify MobileMenu.tsx

--- packages/web-app/src/components/header/mobile/MobileMenu.tsx
+++ packages/web-app/src/components/header/mobile/MobileMenu.tsx
@@ -1,11 +1,11 @@
 import { NavbarMenu, NavbarMenuItem } from "@nextui-org/navbar";
-import { Button } from "@nextui-org/button";
-import { ApplicationPath } from "../../../pages/router.tsx";
 import { DispatchWithoutAction, FC } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Link } from "@nextui-org/link";
 import { Divider } from "@nextui-org/divider";
 import { Item } from "../Header.tsx";
+import { AuthCTA } from "./AuthCTA.tsx";
+import { ApplicationPath } from "../../../pages/router.tsx";
 
 interface Props {
   items: Item[];
@@ -34,31 +34,16 @@ export const MobileMenu: FC<Props> = ({ items, toggleMenu }) => {
       ))}
       <Divider />
       <section className={"flex flex-wrap gap-2 items-center w-full"}>
-        <NavbarMenuItem className={"w-full basis-40 grow"}>
-          <Button
-            color="primary"
-            variant="flat"
-            className={"w-full"}
-            onPress={() => {
-              toggleMenu();
-              navigate(ApplicationPath.LOGIN);
-            }}
-          >
-            Login
-          </Button>
-        </NavbarMenuItem>
-        <NavbarMenuItem className={"w-full basis-40 grow"}>
-          <Button
-            color="primary"
-            className={"w-full"}
-            onPress={() => {
-              toggleMenu();
-              return navigate(ApplicationPath.REGISTER);
-            }}
-          >
-            Register
-          </Button>
-        </NavbarMenuItem>
+        <AuthCTA
+          to={ApplicationPath.LOGIN}
+          toggleMenu={toggleMenu}
+          variant={"flat"}
+        >
+          Login
+        </AuthCTA>
+        <AuthCTA to={ApplicationPath.REGISTER} toggleMenu={toggleMenu}>
+          Register
+        </AuthCTA>
       </section>
     </NavbarMenu>
   );
