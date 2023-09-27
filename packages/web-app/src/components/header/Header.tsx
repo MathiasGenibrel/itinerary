4bcc1e5fc562e2071f1aead61ba5841d22db04ba
Mathias Genibrel <mathias.geni@gmail.com>
2023-09-27 22:14:40 +0200

    🩹 Header is now mobile friendly

--- packages/web-app/src/components/header/Header.tsx
+++ packages/web-app/src/components/header/Header.tsx
@@ -1,59 +1,119 @@
-'use client';
 import {
   Navbar,
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-} from '@nextui-org/navbar';
-import { Button } from '@nextui-org/button';
-// import { Link } from '@nextui-org/link';
-import { Image } from '@nextui-org/image';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { ApplicationPath } from '../../pages/router';
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+} from "@nextui-org/navbar";
+import { Divider } from "@nextui-org/divider";
+import { Link } from "@nextui-org/link";
+import { Button } from "@nextui-org/button";
+import { Image } from "@nextui-org/image";
+import { useLocation, useNavigate } from "react-router-dom";
+import { ApplicationPath } from "../../pages/router";
+import { useReducer } from "react";
 
-export function Header() {
-  const [active, setActive] = useState<string>('home');
+interface Item {
+  name: string;
+  path: ApplicationPath;
+}
+
+export const Header = () => {
+  const [isOpen, toggleMenu] = useReducer((current) => !current, false);
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const menuItems: Item[] = [
+    {
+      name: "Dashboard",
+      path: ApplicationPath.DASHBOARD,
+    },
+    {
+      name: "Profile",
+      path: ApplicationPath.PROFILE,
+    },
+  ];
 
   return (
-    <Navbar>
+    <Navbar isMenuOpen={isOpen} onMenuOpenChange={toggleMenu}>
       <NavbarBrand>
-        <Link to={ApplicationPath.HOME} aria-current="page">
+        <Link href={ApplicationPath.HOME}>
           <Image src="/logo.svg" alt="Logo" width={75} height={75} />
         </Link>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-3" justify="center">
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={item.path === pathname ? "primary" : "foreground"}
+              className={"w-full cursor-pointer"}
+              onClick={() => navigate(item.path)}
+            >
+              {item.name}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarContent>
+
+      <NavbarContent justify="end" className={"hidden sm:flex"}>
         <NavbarItem>
-          <Link to={ApplicationPath.HOME} aria-current="page">
-            Home
-          </Link>
+          <Link href={ApplicationPath.LOGIN}>Login</Link>
         </NavbarItem>
         <NavbarItem>
-          <Link to={ApplicationPath.DASHBOARD} aria-current="page">
-            Dashboard
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link to={ApplicationPath.PROFILE} aria-current="page">
-            Profile
+          <Link href={ApplicationPath.REGISTER}>
+            <Button color="primary">Register</Button>
           </Link>
         </NavbarItem>
       </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem>
-          <Link to={ApplicationPath.LOGIN}>Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link to={ApplicationPath.REGISTER} aria-current="page">
+
+      <NavbarMenuToggle className="sm:hidden" />
+
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={item.path === pathname ? "primary" : "foreground"}
+              className={"w-full cursor-pointer"}
+              onClick={() => {
+                toggleMenu();
+                navigate(item.path);
+              }}
+            >
+              {item.name}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+        <Divider />
+        <section className={"flex flex-wrap gap-2 items-center w-full"}>
+          <NavbarMenuItem className={"w-full basis-40 grow"}>
             <Button
+              color="primary"
               variant="flat"
-              className="rounded-3xl bg-blue-600 text-white"
+              className={"w-full"}
+              onPress={() => {
+                toggleMenu();
+                navigate(ApplicationPath.LOGIN);
+              }}
+            >
+              Login
+            </Button>
+          </NavbarMenuItem>
+          <NavbarMenuItem className={"w-full basis-40 grow"}>
+            <Button
+              color="primary"
+              className={"w-full"}
+              onPress={() => {
+                toggleMenu();
+                return navigate(ApplicationPath.REGISTER);
+              }}
             >
               Register
             </Button>
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
+          </NavbarMenuItem>
+        </section>
+      </NavbarMenu>
     </Navbar>
   );
-}
+};
