0f2b5ad3eebb27072ad91def0ddc03363c732c8f
TommyPREEL <preel.tommy@gmail.com>
2023-10-01 21:49:01 +0200

    travel back ok

--- packages/travel-service/src/index.ts
+++ packages/travel-service/src/index.ts
@@ -74,29 +74,31 @@ app.get('/api/travel/:id', verifyTokenMiddleware, async (req : any, res) => {
   } catch (error) { res.status(500).json({ error: 'Internal server error' });}
 })
 
-app.get('/api/travel/all/:idUser', verifyTokenMiddleware, async (req, res) => {
+app.get('/api/travel/all/:idUser', verifyTokenMiddleware, async (req: any, res) => {
   //TODO: check inputs
   try {
   let allTravels = await travelRepository.find({where : {idUser: Number(req.params.idUser)}})
     const promisesInfo = allTravels.map(async (travel) => {
-      try {
-        const responseStartPoint = await fetch(
-          `https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/records?limit=-1&refine=nom_arrondissement_communes%3A%22Paris%22&refine=is_installed%3A%22OUI%22&refine=stationcode%3A%22${travel.startPoint}%22`
-        );
+      if(travel && (travel.idUser == req.user.id)){
+        try {
+          const responseStartPoint = await fetch(
+            `https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/records?limit=-1&refine=nom_arrondissement_communes%3A%22Paris%22&refine=is_installed%3A%22OUI%22&refine=stationcode%3A%22${travel.startPoint}%22`
+          );
 
-        const responseEndPoint = await fetch(
-          `https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/records?limit=-1&refine=nom_arrondissement_communes%3A%22Paris%22&refine=is_installed%3A%22OUI%22&refine=stationcode%3A%22${travel.endPoint}%22`
-        );
-        const dataStartPoint = await responseStartPoint.json();
-        const dataEndPoint = await responseEndPoint.json();
+          const responseEndPoint = await fetch(
+            `https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/records?limit=-1&refine=nom_arrondissement_communes%3A%22Paris%22&refine=is_installed%3A%22OUI%22&refine=stationcode%3A%22${travel.endPoint}%22`
+          );
+          const dataStartPoint = await responseStartPoint.json();
+          const dataEndPoint = await responseEndPoint.json();
 
-        travel.startPoint = dataStartPoint.results[0].coordonnees_geo
-        travel.endPoint = dataEndPoint.results[0].coordonnees_geo
-        // delete travel.idUser;
-      } catch (error) {
-        console.error('Error fetching bike data', error);
-        res.status(500).json({ error: 'Internal server error' });
-      }
+          travel.startPoint = dataStartPoint.results[0].coordonnees_geo
+          travel.endPoint = dataEndPoint.results[0].coordonnees_geo
+          // delete travel.idUser;
+        } catch (error) {
+          console.error('Error fetching bike data', error);
+          res.status(500).json({ error: 'Internal server error' });
+        }
+    } else { res.status(401).send(); }
     });
     await Promise.all(promisesInfo);
     res.json(allTravels)
