c107b221e14847f7a3ff3dcd8128b6286cee490c
Mathias Genibrel <mathias.geni@gmail.com>
2023-10-01 23:43:02 +0200

    ✨ Download travel

--- packages/pdf-service/src/main.ts
+++ packages/pdf-service/src/main.ts
@@ -1,18 +1,26 @@
 // main.ts
 
-import express from 'express';
-import mapRoutes from './routes/mapRoutes';
+import express from "express";
+import mapRoutes from "./routes/mapRoutes";
+import cors from "cors";
 
 const app = express();
 
 // Middleware pour parser le corps des requêtes en JSON
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: "POST",
+    credentials: true,
+  }),
+);
 
 // Montez vos routes
-app.use('/api/map', mapRoutes);
+app.use("/api/map", mapRoutes);
 
 // Autres routes et configurations
 
-app.listen(5000, () => {
-  console.log('Serveur en écoute sur le port 5000');
+app.listen(5001, () => {
+  console.log("Serveur en écoute sur le port 5001");
 });
